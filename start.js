1a6f9c2d6bb9cd8e53f2be4b9df986ffe5df662e
Joshua Pena <penacalata@gmail.com>
2016-03-02 10:29:14 -0800

    orange

--- start.js
+++ start.js
@@ -73,6 +73,7 @@ RedwoodArrowSecurities.controller("ASStartController",
 				max: $scope.cash,
 	 			value: $scope.cash
 	 		});
+			$("ui-progressbar-value ui-widget-header ui-corner-left").css("background-color", "green");
 
 	 		$(".asset-x").slider({
 	 			range: "min",
