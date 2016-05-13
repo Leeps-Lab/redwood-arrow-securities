733194d576918ecf9d9d7b45acc632930a01f942
Joshua Pena <penacalata@gmail.com>
2016-05-13 02:40:21 -0700

    adding finish pages

---
+++ finish.js
@@ -0,0 +1,34 @@
+RedwoodArrowSecurities.controller("ASFinishController",
+	["$scope",
+	 "RedwoodSubject",
+	 function ($scope, rs) {
+		 $scope.results = [];
+
+		 rs.on_load(function() {
+			 var results = rs.subject[rs.user_id].data["rp.results"];
+
+			 if (!results) {
+				 return;
+			 }
+			 for (var i = 0; i < results.length; i++) {
+				 var result = results[i];
+				 var period = result.period;
+
+				 $scope.results.push({
+					 period: period,
+					 x: result.x * result.Px,
+					 y: result.y * result.Py,
+					 chosen: "",
+					 choosenLabel: "",
+					 selected: false
+				 });
+				 rs.send("__set_points__", {period: period, points: 0});
+			 }
+
+			 rs.send("__set_show_up_fee__", {show_up_fee: 7.0});
+			 rs.send("__set_conversion_rate__", {conversion_rate: 1/3});
+
+			 $scope.labelX = rs.configs[rs.configs.length - 1].labelX || "X";
+			 $scope.labelY = rs.configs[rs.configs.length - 1].labelY || "Y";
+		 });
+}]);
